0bf00b30bea80a29f4694d773c0a54ba07ac1420
swarnacse19 <sharna.cse9.bu@gmail.com>
2025-07-18 23:02:11 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const admin = require("firebase-admin");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+
+const serviceAccount = JSON.parse(decoded);
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
 
@@ -12,8 +17,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-const serviceAccount = require("./food-donation-sdk.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -31,7 +34,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("FoodLinkDB");
     const usersCollection = db.collection("users");
@@ -964,10 +967,10 @@ async function run() {
       }
     );
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // await client.close();
   }
